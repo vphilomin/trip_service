20a6807735663d8af9d1d1b66561ac72f864b96a
Vasi Philomin <vasi@cs.umd.edu>
2020-09-27 20:54:00 -0700

    Add a complete set of test for TripService using mocks and stubs

--- spec/trip_service_spec.rb
+++ spec/trip_service_spec.rb
@@ -1,5 +1,43 @@
 require 'rspec'
 require_relative '../trip/trip_service'
+require_relative '../trip/trip'
 
 describe TripService do
+  let(:trip_service) { TripService.new }
+  let(:session) { class_double(UserSession).as_stubbed_const }
+
+  context "when no user is logged in" do
+    before do
+      session_instance = instance_double(UserSession, :get_logged_user => nil)
+      allow(session).to receive(:get_instance).and_return(session_instance)
+    end
+
+    it "raises a not logged in exception" do
+      expect { trip_service.get_trip_by_user(User.new) }.to raise_error(UserNotLoggedInException)
+    end
+  end
+
+  context "when user is logged in" do
+    let(:logged_user) { User.new }
+    let(:user) { User.new }
+    let(:trip) { Trip.new }
+
+    before do
+      session_instance = instance_double(UserSession, :get_logged_user => logged_user)
+      allow(session).to receive(:get_instance).and_return(session_instance)
+    end
+
+    it "returns an empty trip list when requesting trips of a user that is not a friend" do
+      trip_list = trip_service.get_trip_by_user(user)
+      expect(trip_list).to be_empty
+    end
+
+    it "returns the actual trips of a friend user" do
+      user.add_friend(logged_user)
+      allow(TripDAO).to receive(:find_trips_by_user).with(user).and_return([trip])
+
+      trip_list = trip_service.get_trip_by_user(user)
+      expect(trip_list).to contain_exactly(trip)
+    end
+  end
 end
--- trip/trip_service.rb
+++ trip/trip_service.rb
@@ -5,29 +5,26 @@ require_relative '../еxceptions/user_not_logged_in_exception'
 require_relative '../еxceptions/dependent_class_call_during_unit_test_exception'
 
 class TripService
+  def get_trip_by_user(user)
+    trip_list = []
+    logged_user = UserSession.get_instance.get_logged_user
+    is_friend = false
 
-    def get_trip_by_user(user)
-        trip_list = []
-        logged_user = UserSession.get_instance.get_logged_user
-        is_friend = false
-
-        if(!logged_user.nil?)
-            user.get_friends.each do |f|
-                if(f.equal? logged_user)
-                    is_friend = true
-                    break
-                end
-            end
-
-            if(is_friend)
-                trip_list = TripDAO.find_trips_by_user(user)
-            end
-
-            return trip_list
-        else
-            raise UserNotLoggedInException.new
+    if(!logged_user.nil?)
+      user.get_friends.each do |f|
+        if(f.equal? logged_user)
+          is_friend = true
+          break
         end
+      end
 
+      if(is_friend)
+        trip_list = TripDAO.find_trips_by_user(user)
+      end
+
+      return trip_list
+    else
+      raise UserNotLoggedInException.new
     end
-
+  end
 end
