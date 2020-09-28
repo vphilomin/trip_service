658439b0d82cbd868479c6300d9484426b3a67ec
Vasi Philomin <vasi@cs.umd.edu>
2020-09-27 22:06:42 -0700

    Inject logged_user and dramatically simplify tests and code

--- spec/trip_service_spec.rb
+++ spec/trip_service_spec.rb
@@ -7,13 +7,8 @@ describe TripService do
   let(:session) { class_double(UserSession).as_stubbed_const }
 
   context "when no user is logged in" do
-    before do
-      session_instance = instance_double(UserSession, :get_logged_user => nil)
-      allow(session).to receive(:get_instance).and_return(session_instance)
-    end
-
     it "raises a not logged in exception" do
-      expect { trip_service.get_trip_by_user(User.new) }.to raise_error(UserNotLoggedInException)
+      expect { trip_service.get_trip_by_user(User.new, logged_user: nil) }.to raise_error(UserNotLoggedInException)
     end
   end
 
@@ -23,20 +18,17 @@ describe TripService do
     let(:trip) { Trip.new }
 
     before do
-      session_instance = instance_double(UserSession, :get_logged_user => logged_user)
-      allow(session).to receive(:get_instance).and_return(session_instance)
       allow(TripDAO).to receive(:find_trips_by_user).with(user).and_return([trip])
     end
 
     it "returns an empty trip list when requesting trips of a user that is not a friend" do
-      trip_list = trip_service.get_trip_by_user(user)
+      trip_list = trip_service.get_trip_by_user(user, logged_user: logged_user)
       expect(trip_list).to be_empty
     end
 
     it "returns the actual trips of a friend user" do
       allow(user).to receive(:friend?).with(logged_user).and_return(true)
-
-      trip_list = trip_service.get_trip_by_user(user)
+      trip_list = trip_service.get_trip_by_user(user, logged_user: logged_user)
       expect(trip_list).to contain_exactly(trip)
     end
   end
--- trip/trip_service.rb
+++ trip/trip_service.rb
@@ -5,8 +5,7 @@ require_relative '../еxceptions/user_not_logged_in_exception'
 require_relative '../еxceptions/dependent_class_call_during_unit_test_exception'
 
 class TripService
-  def get_trip_by_user(user)
-    logged_user = UserSession.get_instance.get_logged_user
+  def get_trip_by_user(user, logged_user: UserSession.get_instance.get_logged_user)
     raise UserNotLoggedInException.new if logged_user.nil?
 
     trip_list = []
